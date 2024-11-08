c990498beaf175285b65b93615d9fc23bd567eef
Galoniax <axelian2504@gmail.com>
2024-11-08 02:05:13 -0300

    Update general´s things

--- SoundWave/src/App.jsx
+++ SoundWave/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./views/home/home";
+
+import { Navbar } from "./layout/navbar/navbar";
 import './App.css'
 
+const routes = [
+  {path: "/", name: "Inicio", component: <Home/>}
+]
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <Router>
+      <Navbar />
+
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={route.component}
+          />
+        ))}
+      </Routes>
+    </Router>
+
     </>
-  )
+    
+  );
 }
 
 export default App
